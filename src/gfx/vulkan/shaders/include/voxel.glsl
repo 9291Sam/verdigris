d44950570ef6b60ef689c5605af163f4befaee5e
Sam Skinner <sebs@Sams-MacBook-Pro-2.local>
2023-11-03 17:39:10 -0400

    work on voxel structures

---
+++ src/gfx/vulkan/shaders/include/voxel.glsl
@@ -0,0 +1,21 @@
+#extension GL_EXT_shader_explicit_arithmetic_types : enable
+#extension GL_EXT_shader_explicit_arithmetic_types_int8 : enable
+
+struct Voxel
+{
+    uint8_t srgb_r;
+    uint8_t srgb_g;
+    uint8_t srgb_b;
+
+    uint8_t alpha_or_emissive;
+
+    // TODO: there's too much information here
+    uint8_t specular;
+    uint8_t roughness;
+    uint8_t metallic;
+
+    /// 0 - nothing special
+    /// 1 - anisotropic
+    /// [2, 255] UB
+    uint8_t special;
+};
--- src/gfx/vulkan/voxel/storage_manager.hpp
+++ src/gfx/vulkan/voxel/storage_manager.hpp
@@ -1,10 +1,59 @@
 #ifndef SRC_GFX_VULKAN_VOXEL_STORAGE_MANAGER_HPP
 #define SRC_GFX_VULKAN_VOXEL_STORAGE_MANAGER_HPP
 
+#include <gfx/vulkan/buffer.hpp>
+
 namespace gfx::vulkan::voxel
 {
-    class StorageManager
-    {};
+    /// Implementation a similar scheme
+    ///
+
+    /// Explanation
+    /// One massive, linear buffer called the brick buffer voxel
+    /// this is a massive, entierly linear buffer split into chunks
+    /// this is entierly managed by the cpu
+    /// modifications to it are managed by the cpu and the cpu keeps a list of
+    /// buffer, when one needs to be removed, its cleared and marked as clear
+    /// and used for new allocations
+
+    class VoxelStorageManager
+    {
+    public:
+        struct Voxel
+        {
+            std::uint8_t srgb_r;
+            std::uint8_t srgb_g;
+            std::uint8_t srgb_b;
+
+            // 0 -> 127 represent varying levels of transparency 128 -> 255
+            // represent emissive / light emissivity strength
+            std::uint8_t alpha;
+
+            std::uint8_t specular;
+            std::uint8_t roughness;
+            std::uint8_t metallic;
+            std::uint8_t anisotropic; // / special, this is going to be shoved
+                                      // into a switch case
+        };
+        static_assert(sizeof(Voxel) == sizeof(std::uint64_t));
+
+        struct Brick
+        {
+            static constexpr std::size_t Dimension = 8;
+
+            std::array<Voxel, Dimension * Dimension * Dimension> voxels;
+        };
+
+        static_assert(sizeof(Brick) == 4096);
+        static_assert(std::is_trivially_copyable_v<Brick>);
+
+    public:
+
+    private:
+
+        vulkan::Buffer brick_buffer;
+        std::set
+    };
 }; // namespace gfx::vulkan::voxel
 
 #endif // SRC_GFX_VULKAN_VOXEL_STORAGE_MANAGER_HPP
