74e67dee9452ab2a1fffb31f19684cf6a7236ad3
Sam Skinner <sebs@Sams-MacBook-Pro-2.local>
2023-11-05 09:24:32 -0500

    Added cpu linear brick allocator

--- src/gfx/vulkan/shaders/include/ray.glsl
+++ src/gfx/vulkan/shaders/include/ray.glsl
@@ -1,3 +1,6 @@
+#ifndef SRC_GFX_VULKAN_SHADERS_INCLUDE_RAY_GLSL
+#define SRC_GFX_VULKAN_SHADERS_INCLUDE_RAY_GLSL
+
 struct Ray
 {
     vec3 origin;
@@ -8,3 +11,5 @@ vec3 Ray_at(in Ray self, in float t)
 {
     return self.origin + self.direction * t;
 }
+
+#endif // SRC_GFX_VULKAN_SHADERS_INCLUDE_RAY_GLSL
