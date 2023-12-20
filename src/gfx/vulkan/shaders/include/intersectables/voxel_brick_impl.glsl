c6602223cb0e6e17e7e482e309a9b43d6fa39d1e
9291Sam <sam.skinr@gmail.com>
2023-12-19 21:44:10 -0500

    Working DDA Algorithm!

--- src/gfx/vulkan/shaders/include/intersectables/voxel_brick_impl.glsl
+++ src/gfx/vulkan/shaders/include/intersectables/voxel_brick_impl.glsl
@@ -150,7 +150,144 @@
 // result.maybe_color           = vec4(
 //     vec3(voxelStartIndexChecked) / float(VoxelBrick_EdgeLength), 1.0);
 
-// return result;
+float sdSphere(vec3 p, float d)
+{
+    return length(p) - d;
+}
+
+float sdBox(vec3 p, vec3 b)
+{
+    vec3 d = abs(p) - b;
+    return min(max(d.x, max(d.y, d.z)), 0.0) + length(max(d, 0.0));
+}
+
+bool getVoxel(ivec3 c)
+{
+    vec3  p = vec3(c) + vec3(0.5);
+    float d =
+        min(max(-sdSphere(p, 7.5), sdBox(p, vec3(6.0))), -sdSphere(p, 25.0));
+    return d < 0.0;
+}
+
+IntersectionResult VoxelBrick_tryIntersect21(
+    const uint offset, const vec3 cornerPos, const Ray ray)
+{
+    const bool USE_BRANCHLESS_DDA = true;
+    const int  MAX_RAY_STEPS      = 128;
+
+    vec3 rayDir = ray.direction;
+    vec3 rayPos = ray.origin;
+
+    ivec3 mapPos = ivec3(floor(rayPos + 0.));
+
+    vec3 deltaDist = abs(vec3(length(rayDir)) / rayDir);
+
+    ivec3 rayStep = ivec3(sign(rayDir));
+
+    vec3 sideDist =
+        (sign(rayDir) * (vec3(mapPos) - rayPos) + (sign(rayDir) * 0.5) + 0.5)
+        * deltaDist;
+
+    bvec3 mask;
+
+    for (int i = 0; i < MAX_RAY_STEPS; i++)
+    {
+        if (getVoxel(mapPos))
+        {
+            continue;
+        }
+
+        if (USE_BRANCHLESS_DDA)
+        {
+            // Thanks kzy for the suggestion!
+            mask = lessThanEqual(sideDist.xyz, min(sideDist.yzx, sideDist.zxy));
+            /*bvec3 b1 = lessThan(sideDist.xyz, sideDist.yzx);
+            bvec3 b2 = lessThanEqual(sideDist.xyz, sideDist.zxy);
+            mask.x = b1.x && b2.x;
+            mask.y = b1.y && b2.y;
+            mask.z = b1.z && b2.z;*/
+            // Would've done mask = b1 && b2 but the compiler is making me do it
+            // component wise.
+
+            // All components of mask are false except for the corresponding
+            // largest component of sideDist, which is the axis along which
+            // the ray should be incremented.
+
+            sideDist += vec3(mask) * deltaDist;
+            mapPos += ivec3(vec3(mask)) * rayStep;
+        }
+        else
+        {
+            if (sideDist.x < sideDist.y)
+            {
+                if (sideDist.x < sideDist.z)
+                {
+                    sideDist.x += deltaDist.x;
+                    mapPos.x += rayStep.x;
+                    mask = bvec3(true, false, false);
+                }
+                else
+                {
+                    sideDist.z += deltaDist.z;
+                    mapPos.z += rayStep.z;
+                    mask = bvec3(false, false, true);
+                }
+            }
+            else
+            {
+                if (sideDist.y < sideDist.z)
+                {
+                    sideDist.y += deltaDist.y;
+                    mapPos.y += rayStep.y;
+                    mask = bvec3(false, true, false);
+                }
+                else
+                {
+                    sideDist.z += deltaDist.z;
+                    mapPos.z += rayStep.z;
+                    mask = bvec3(false, false, true);
+                }
+            }
+        }
+    }
+
+    vec3 color;
+    if (mask.x)
+    {
+        color = vec3(0.5);
+    }
+    if (mask.y)
+    {
+        color = vec3(1.0);
+    }
+    if (mask.z)
+    {
+        color = vec3(0.75);
+    }
+    if (getVoxel(mapPos))
+    {
+        Cube cube;
+        cube.center      = mapPos * 1.0 + cornerPos + Voxel_Size / 2.0;
+        cube.edge_length = 1.0;
+
+        IntersectionResult result = Cube_tryIntersect(cube, ray);
+
+        result.maybe_color = Voxel_getLinearColor(
+            VOXEL_BRICK_IMPL_ARRAY[offset]
+                .voxels[mapPos.x % 8][mapPos.y % 8][mapPos.z % 8]);
+
+        if (result.intersection_occurred)
+        {
+            return result;
+        }
+    }
+    else
+    {
+        return IntersectionResult_getMiss();
+    }
+    // fragColor.rgb = vec3(mapPos) / 9.0;
+    // fragColor.rgb = vec3(0.1 * noiseDeriv);
+}
 
 IntersectionResult
 VoxelBrick_tryIntersect2(const uint offset, const vec3 cornerPos, const Ray ray)
