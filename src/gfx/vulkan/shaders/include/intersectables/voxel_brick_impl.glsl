2916436f015de1cec006eec323699b0f66a563b0
9291Sam <sam.skinr@gmail.com>
2023-12-24 21:54:11 -0500

    Finished shader cleanup (for now)

--- src/gfx/vulkan/shaders/include/intersectables/voxel_brick_impl.glsl
+++ src/gfx/vulkan/shaders/include/intersectables/voxel_brick_impl.glsl
@@ -7,36 +7,9 @@
 #error VOXEL_BRICK_IMPL_ARRAY must be defined
 #endif //  VOXEL_BRICK_IMPL_ARRAY
 
-float sdSphere(vec3 p, float d)
+IntersectionResult
+VoxelBrick_tryIntersect(const uint offset, const vec3 cornerPos, const Ray ray)
 {
-    return length(p) - d;
-}
-
-float sdBox(vec3 p, vec3 b)
-{
-    vec3 d = abs(p) - b;
-    return min(max(d.x, max(d.y, d.z)), 0.0) + length(max(d, 0.0));
-}
-
-bool getVoxel(uint offset, ivec3 c)
-{
-    // vec3  p = vec3(c) + vec3(0.5);
-    // float d =
-    //     min(max(-sdSphere(p, 7.5), sdBox(p, vec3(6.0))), -sdSphere(p, 25.0));
-    // return d < 0.0;
-    if (any(lessThan(c, ivec3(0))) || any(greaterThan(c, ivec3(7))))
-    {
-        return false;
-    }
-
-    return Voxel_isVisible(
-        VOXEL_BRICK_IMPL_ARRAY[offset].voxels[c.x][c.y][c.z]);
-}
-
-IntersectionResult VoxelBrick_tryIntersect21(
-    const uint offset, const vec3 cornerPos, const Ray ray)
-{
-    // routine to jump to the start of the cube
     Cube boundingCube;
     boundingCube.center      = cornerPos + VoxelBrick_EdgeLength / 2;
     boundingCube.edge_length = VoxelBrick_EdgeLength;
@@ -67,15 +40,15 @@ IntersectionResult VoxelBrick_tryIntersect21(
 
     vec3 rayDir = ray.direction;
 
-    ivec3 mapPos = ivec3(floor(rayPos + 0.));
+    ivec3 localVoxelPosition = ivec3(floor(rayPos + 0.));
 
     vec3 deltaDist = abs(vec3(length(rayDir)) / rayDir);
 
     ivec3 rayStep = ivec3(sign(rayDir));
 
-    vec3 sideDist =
-        (sign(rayDir) * (vec3(mapPos) - rayPos) + (sign(rayDir) * 0.5) + 0.5)
-        * deltaDist;
+    vec3 sideDist = (sign(rayDir) * (vec3(localVoxelPosition) - rayPos)
+                     + (sign(rayDir) * 0.5) + 0.5)
+                  * deltaDist;
 
     bvec3 mask;
 
@@ -85,8 +58,9 @@ IntersectionResult VoxelBrick_tryIntersect21(
         // For accruacy reasons we do this in two stages
         // If we've traversed COMPLETELY outside of the cube (outside by more
         // than 2) we know its a miss
-        if (any(lessThan(mapPos, ivec3(-1)))
-            || any(greaterThan(mapPos, ivec3(VoxelBrick_EdgeLength))))
+        if (any(lessThan(localVoxelPosition, ivec3(-1)))
+            || any(
+                greaterThan(localVoxelPosition, ivec3(VoxelBrick_EdgeLength))))
         {
             return IntersectionResult_getMiss();
         }
@@ -94,20 +68,25 @@ IntersectionResult VoxelBrick_tryIntersect21(
         // Otherwise, we may be on the cusp of the cube and only outside by one,
         // but because of floating point errors we may come back in
         // This is the nasty condition we need.
-        if (all(greaterThanEqual(mapPos, ivec3(0)))
-            && all(lessThanEqual(mapPos, ivec3(VoxelBrick_EdgeLength - 1)))
-            && Voxel_isVisible(VOXEL_BRICK_IMPL_ARRAY[offset]
-                                   .voxels[mapPos.x][mapPos.y][mapPos.z]))
+        if (all(greaterThanEqual(localVoxelPosition, ivec3(0)))
+            && all(lessThanEqual(
+                localVoxelPosition, ivec3(VoxelBrick_EdgeLength - 1)))
+            && Voxel_isVisible(
+                VOXEL_BRICK_IMPL_ARRAY[offset]
+                    .voxels[localVoxelPosition.x][localVoxelPosition.y]
+                           [localVoxelPosition.z]))
         {
             Cube cube;
-            cube.center = mapPos * Voxel_Size + cornerPos + Voxel_Size / 2.0;
+            cube.center =
+                localVoxelPosition * Voxel_Size + cornerPos + Voxel_Size / 2.0;
             cube.edge_length = 1.0;
 
             IntersectionResult result = Cube_tryIntersect(cube, ray);
 
-            result.maybe_color =
-                Voxel_getLinearColor(VOXEL_BRICK_IMPL_ARRAY[offset]
-                                         .voxels[mapPos.x][mapPos.y][mapPos.z]);
+            result.maybe_color = Voxel_getLinearColor(
+                VOXEL_BRICK_IMPL_ARRAY[offset]
+                    .voxels[localVoxelPosition.x][localVoxelPosition.y]
+                           [localVoxelPosition.z]);
 
             if (result.intersection_occurred)
             {
@@ -120,16 +99,18 @@ IntersectionResult VoxelBrick_tryIntersect21(
             // }
         }
 
+        // I'm not even going to pretend I know what's going on here.
+        // Adapted from https://www.shadertoy.com/view/4dX3zl
         mask = lessThanEqual(sideDist.xyz, min(sideDist.yzx, sideDist.zxy));
         sideDist += vec3(mask) * deltaDist;
-        mapPos += ivec3(vec3(mask)) * rayStep;
+        localVoxelPosition += ivec3(vec3(mask)) * rayStep;
     }
 
     return IntersectionResult_getMiss();
 }
 
 IntersectionResult
-VoxelBrick_tryIntersect(const uint offset, const vec3 cornerPos, Ray ray)
+VoxelBrick_tryIntersectNaieve(const uint offset, const vec3 cornerPos, Ray ray)
 {
     Cube brickOuterCube;
     brickOuterCube.center = cornerPos
